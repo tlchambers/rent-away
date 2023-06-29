a7bdb93453c3b831adf88cdb4a65b96f716cdadf
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-29 19:59:14 +0100

    initial design of header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
 	return (
 		<div>
-			<header className="p-4">
+			<header className="p-4 flex justify-between">
 				<a href="" className="flex items-center gap-1">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -21,6 +21,30 @@ function App() {
 					</svg>
 					<span className="font-bold text-xl">rent away</span>
 				</a>
+				<div className="flex border border-gray-300 gap-2 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+					<div>Anywhere</div>
+					<div className="border-l border-gray-300"></div>
+					<div>Anyweek</div>
+					<div className="border-l border-gray-300"></div>
+					<div>Any guest</div>
+					<div>Any Guest</div>
+					<button>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+							/>
+						</svg>
+					</button>
+				</div>
 			</header>
 		</div>
 	);
