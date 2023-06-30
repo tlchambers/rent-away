2798ba2b5411297fa5f026dfa360d109e27ccf6e
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-30 23:40:08 +0100

    added register

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
 app.use(
 	cors({
 		credentials: true,
@@ -11,6 +12,11 @@ app.use(
 
 app.get('/test', (req, res) => {
 	res.json('test ok');
+});
+
+app.get('/register', (req, res) => {
+	const { name, email, password } = req.body;
+	res.json({ name, email, password });
 });
 
 app.listen(4000, () =>
