339ca9464e4a9d42c92cf674b8edf9e5f988f1cf
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-06-30 20:33:06 +0100

    added state to form

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function RegisterPage() {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
 		<div className="mt-4 grow flex items-center justify-center">
 			<div className="mb-64">
 				<h1 className="text-4xl text-center mb-4">Register</h1>
 				<form className="max-w-md mx-auto">
-					<input type="text" placeholder="John Smith" />
-					<input type="email" placeholder="your@email.com" />
-					<input type="password" placeholder="password" />
+					<input
+						type="text"
+						placeholder="John Smith"
+						value={name}
+						onChange={(event) => setName(event.target.value)}
+					/>
+					<input
+						type="email"
+						placeholder="your@email.com"
+						value={email}
+						onChange={(event) => setEmail(event.target.value)}
+					/>
+					<input
+						type="password"
+						placeholder="password"
+						value={password}
+						onChange={(event) => setPassword(event.target.value)}
+					/>
 					<button className="custom">Login</button>
 					<div className="text-center py-2 text-gray-500">
 						Already a memeber?{' '}
