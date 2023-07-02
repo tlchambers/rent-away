ba6d01fa90978e3a6c5ef2281844eb8167646b40
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-02 18:18:54 +0100

    added dotenv to db, added bcrypt to password

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
 const app = express();
+const User = require('./models/User.js');
+
+const bcryptSalt = bcrypt.genSaltSync(8);
 
 app.use(express.json());
 app.use(
@@ -15,15 +21,32 @@ app.get('/test', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-	const { name, email, password } = req.body;
-	res.json({ name, email, password });
+	const { first, last, email, password } = req.body;
+	res.json({ first, last, email, password });
 });
 
-app.post('/register', (req, res) => {
-	const { name, email, password } = req.body;
-	res.json({ name, email, password });
+app.post('/register', async (req, res) => {
+	const { first, last, email, password } = req.body;
+	try {
+		const newUser = await User.create({
+			first,
+			last,
+			email,
+			password: bcrypt.hashSync(password, bcryptSalt),
+		});
+
+		console.log('user created');
+		res.json({ newUser });
+	} catch (err) {
+		console.error(err.message);
+		res.json({ statusCode: 500, error: err.message });
+	}
 });
 
 app.listen(4000, () =>
 	console.log(`RENT AWAY currently app listening on port ${4000}!`)
 );
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('Mongodb Connected!'));
