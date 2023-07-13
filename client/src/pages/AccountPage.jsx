74a6bdb95c6c25b7971b631db4307165992b24f8
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-13 21:35:20 +0100

    adding initial user context

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { UserContext } from '../UserContext';
 
 function AccountPage() {
-	return <div>AccountPage</div>;
+	const { user } = UserContext(UserContext);
+	return <div>Account page for {user.name}</div>;
 }
 
 export default AccountPage;
