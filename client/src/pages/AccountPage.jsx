74e41ba7b65d3e03a882d87393ed490c449d1323
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-13 21:37:43 +0100

    adding navigate to login page if not user

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { UserContext } from '../UserContext';
 
 function AccountPage() {
-	const { user } = UserContext(UserContext);
-	return <div>Account page for {user.name}</div>;
+	const { ready, user } = UserContext(UserContext);
+	if (ready && !user) {
+		return <Navigate to={'/login'} />;
+	}
+	return <div>Account page for {user?.name}</div>;
 }
 
 export default AccountPage;
