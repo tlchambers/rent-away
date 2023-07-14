bcb4949fed702a1b8d6b005379a9b8f5924e6e89
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-14 17:21:39 +0100

    initial usercontext

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -2,9 +2,8 @@ import { createContext, useState } from 'react';
 
 export const UserContext = createContext({});
 
-export function UserContextProvider(children) {
+export function UserContextProvider({ children }) {
 	const [user, setUser] = useState(null);
-
 	return (
 		<UserContext.Provider value={{ user, setUser }}>
 			{children}
