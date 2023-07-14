8e01a9f09e43b6c9d7dfca592c10a8059fe39821
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-14 17:22:07 +0100

    debugging usercontext

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { UserContext } from '../UserContext';
+// import { UserContext } from '../UserContext';
 
 function LoginPage() {
 	const [email, setEmail] = useState('');
